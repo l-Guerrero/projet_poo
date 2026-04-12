eec4645c3065272728bcf53737e2de00ca96ee3a
luisa Peña Guerrero <luisafdapg107@gmail.com>
2026-04-12 13:40:46 -0500

    feat(Ejercicio24):Constructor con parametros agregado a la clase libro

--- src/bloqueD/Libro.java
+++ src/bloqueD/Libro.java
@@ -4,8 +4,14 @@ public class Libro {
         String titulo;
         String autor;
 
-    // Aqui está el constructor vacio que pid el ejercicio23
+    // Aqui está el constructor vacio que pide el ejercicio23
     public Libro(){
+    }
+    // constructor con párametros que pide el Ejercicio24
+    public Libro(String tituloparametro, String autorparametro)
+    {
+        this.titulo = tituloparametro;
+        this.autor = autorparametro;
     }
 }
        class MainLibro{
@@ -27,10 +33,17 @@ public class Libro {
             lb4.titulo = "El viejo y el mar";
             lb4.autor = "Ernest Hemingway";
 
+            // usar parametros
+            // mismo autor diferente titulo
+            Libro lb5 = new Libro("El amor en los tiempos del cólera", "Gabriel Garcia Marquez");
+            Libro lb6 = new Libro ("Crónica de una muerte anunciada", "Gabriel Garcia Marquez");
+
             System.out.println("1: " + lb1.titulo + " - " + lb1.autor);
             System.out.println("2: " + lb2.titulo + " - " + lb2.autor);
             System.out.println("3: " + lb3.titulo + " - " + lb3.autor);
             System.out.println("4: " + lb4.titulo + " - " + lb4.autor);
+            System.out.println("5: " + lb5.titulo + " - " + lb5.autor);
+            System.out.println("6: " + lb6.titulo + " - " + lb6.autor);
 
         /* La clase funciona como una base o plantilla porque solo define qué datos debe tener
         cualquier libro para existir en el sistema. Los objetos son los casos reales porque son versiones
