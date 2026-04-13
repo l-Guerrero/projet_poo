f41f8f6da96fd1584607b55b08c44fe735e7c0e0
luisa Peña Guerrero <luisafdapg107@gmail.com>
2026-04-12 21:50:41 -0500

    feat(Ejercicio 31 y 32): Corregí algunas lineas y se creo el método fábrica estático en productoo. Tambien se crearon dos nuevos productos que es lo que pide el ejercicio 32

--- src/bloqueC/Productoo.java
+++ src/bloqueC/Productoo.java
@@ -1,26 +1,33 @@
 package bloqueC;
 
-public class  Productoo {
+public class Productoo {
     String nombre;
     double precio;
     int stock;
 
+    // Ejercicio 31 fábrica estático
+    public static Productoo
+    crearProductoBasico(String nombre, double precio) {
+        Productoo nuevoProducto = new Productoo();
+        nuevoProducto.nombre = nombre;
+        nuevoProducto.precio = precio;
+        nuevoProducto.stock = 5;
+        return nuevoProducto;
+    }
+    public void mostrarProducto() {
+        System.out.println("Detalles del producto:");
+        System.out.println("Nombre:" + nombre);
+        System.out.println("Precio:" + precio);
+        System.out.println("Stock:" + stock);
+    }
     // Ejercicio 23 y 26
     public Productoo() {
-
     }
     // Ejercicio 24 y 26
     public Productoo(String nombre, double precio, int stock) {
         this.nombre = nombre;
         this.precio = precio;
         this.stock = stock;
-    }
-
-    public void mostrarProducto() {
-        System.out.println("Detalles del producto:");
-        System.out.println("Nombre:" + nombre);
-        System.out.println("Precio:" + precio);
-        System.out.println("Stock:" + stock);
     }
 
     // metodo vender y descontar del stock
@@ -69,12 +76,33 @@ class MainProductoo {
             // con parametros
         Productoo tec = new Productoo("teclado", 86000, 35);
 
+        // Ejercicio 32 con el metodo crear dos productos
+        Productoo mar = Productoo.crearProductoBasico("Marcadores", 1700);
+        Productoo car = Productoo.crearProductoBasico("Cartulina", 5000);
+
         // cambios en el inventario
         System.out.println("Movimientos en el iventario");
         b3.vender(20);
         b3.reabastecer(50);
-        b3.vender(20);
 
+        c2.vender(18);
+        c2.reabastecer(30);
+
+        m1.vender(50);
+        m1.reabastecer(100);
+
+        tv.vender(12);
+        tv.reabastecer(30);
+
+        tec.vender(1);
+        tec.reabastecer(10);
+
+        mar.vender(200);
+        mar.reabastecer(300);
+
+        car.vender(150);
+        car.reabastecer(400);
+        
         // resultado final
         System.out.println("Inventario final");
         m1.mostrarProducto();
@@ -82,6 +110,8 @@ class MainProductoo {
         b3.mostrarProducto();
         tv.mostrarProducto();
         tec.mostrarProducto();
+        mar.mostrarProducto();
+        car.mostrarProducto();
     }
 }
 
