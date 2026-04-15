7bbc03708a3da7451f879eb799e071ca6a375968
luisa Peña Guerrero <luisafdapg107@gmail.com>
2026-04-14 19:13:32 -0500

    feat(Ejercicio 60): La clase Avión tiene una función para que puedan abordar más pasajeros al vuelo y luego se suma y da el total

---
+++ src/BloqueJ/Avion.java
@@ -0,0 +1,28 @@
+package BloqueJ;
+
+public class Avion {
+        String aerolinea;
+        String destino;
+        int pasajeros;
+
+        public Avion(String aerolinea, String destino, int pasajeros) {
+            this.aerolinea = aerolinea;
+            this.destino = destino;
+            this.pasajeros = pasajeros;
+        }
+
+        // sumar pasajeros
+        public void abordar(int cantidad) {
+            this.pasajeros += cantidad;
+            System.out.println("Han subido " + cantidad + " pasajeros nuevos al vuelo de " + aerolinea);
+        }
+    }
+         class MainAvion {
+        public static void main(String[] args) {
+
+            Avion vuelo = new Avion("Avianca", "Cartagena", 80);
+            vuelo.abordar(55);
+            System.out.println("Total de pasajeros hacia " + vuelo.destino + ": " + vuelo.pasajeros);
+        }
+    }
+
