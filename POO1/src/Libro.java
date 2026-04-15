a44dc723ea7ef26ec966f1b6d3ee34d99059d253
luisa Peña Guerrero <luisafdapg107@gmail.com>
2026-04-14 22:39:26 -0500

    feat(ejercicio 1):implementada la clase libro con constructores por defecto y parmetrizado

--- POO1/src/Libro.java
+++ POO1/src/Libro.java
@@ -1,3 +1,30 @@
 public class Libro {
+        String titulo;
+        String autor;
+        int numeroPaginas;
 
-}
+        public Libro() {
+            this.titulo = "Sin titulo";
+            this.autor = "Desconocido";
+            this.numeroPaginas = 0;
+        }
+
+        public Libro(String titulo, String autor, int numeroPaginas) {
+            this.titulo = titulo;
+            this.autor = autor;
+            this.numeroPaginas = numeroPaginas;
+        }
+
+        public void mostrarDetalles() {
+            System.out.println("Libro: " + titulo + " Autor: " + autor + "  Paginas: " + numeroPaginas);
+        }
+
+        public static void main(String[] args) {
+            Libro libro1 = new Libro();
+            Libro libro2 = new Libro("Cien años de soledad", "Gabriel Garcia Marquez", 550);
+
+            libro1.mostrarDetalles();
+            libro2.mostrarDetalles();
+        }
+    }
+
